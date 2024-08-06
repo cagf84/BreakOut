e2bc44500b76799072513fc551bd1617a0ae1b59
cagf84 <cagf.02@gmail.com>
2024-08-06 00:01:00 -0400

    Tarea-Modulo-10v2
    Tarea-Modulo-10v2

--- Assets/_Script/Bloque.cs
+++ Assets/_Script/Bloque.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Bloque : MonoBehaviour
 {
+
     public int resistencia = 1;
     // Start is called before the first frame update
     void Start()
--- Assets/_Script/Bloque_Madera.cs
+++ Assets/_Script/Bloque_Madera.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bloque_Madera : Bloque
 {
 
+
     // Start is called before the first frame update
     void Start()
     {
